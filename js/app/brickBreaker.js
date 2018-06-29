ded2d342e916e93e0aac78fc7a210bf9c5cf0dac
Caleb Thornton <caleb.thornton@LOH056MP80AFD57.cardinalhealth.net>
2018-06-29 13:38:36 -0400

    incremental changes for restructure that will allow for customization of stages

--- js/app/brickBreaker.js
+++ js/app/brickBreaker.js
@@ -6,46 +6,15 @@ var x = canvas.width/2;
 var y = canvas.height/2;
 var dx = 2;
 var dy = -2;
-var ballRadius = 10;
-var paddleWidth = 75;
-var paddleHeight = 10;
-var paddleX = (canvas.width-paddleWidth)/2;
-var paddleY = (canvas.height-paddleHeight);
 var rightPressed = false;
 var leftPressed = false;
 var gameOver = false;
 var score = 0;
-//Birck Vars
-var brickRowCount = 3;
-var brickColumnCount = 5;
-var brickWidth = 75;
-var brickHeight = 20;
-var brickPadding = 10;
-var brickOffsetTop = 30;
-var brickOffsetLeft = 30; //figure out mathmatical scaling for centering
 var bricks = [];
 for(c=0; c<brickColumnCount; c++){
   bricks[c] = [];
   for(r=0; r<brickRowCount; r++){
     bricks[c][r] = {x:0, y:0, status: 1};
-  }
-}
-
-function draw(){
-  if(gameOver == true){
-    endGame(loseText)
-  }
-  else{
-    ctx.clearRect(0,0, canvas.width, canvas.height);
-    drawBall();
-    drawPaddle();
-    drawBricks();
-    collisionDetection();
-    movePaddle();
-    checkScore();
-    x += dx;
-    y += dy;
-    //requestAnimationFrame(draw);
   }
 }
 
@@ -104,50 +73,23 @@ function brickIsActive(brick){
   return (brick.status == 1);
 }
 
-function drawBall(){
-  ctx.beginPath();
-  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-  ctx.fillStyle = "#0095DD";
-  ctx.fill();
-  ctx.closePath();
-}
-
-function drawPaddle(){
-  ctx.beginPath();
-  ctx.rect(paddleX, paddleY, paddleWidth, paddleHeight);
-  ctx.fillStyle = "#0095DD";
-  ctx.fill();
-  ctx.closePath();
-}
-
-function drawBricks(){
-  for(c=0; c<brickColumnCount; c++) {
-    for(r=0; r<brickRowCount; r++) {
-      if(bricks[c][r].status == 1){
-        var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
-        var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
-        bricks[c][r].x = brickX;
-        bricks[c][r].y = brickY;
-        ctx.beginPath();
-        ctx.rect(brickX, brickY, brickWidth, brickHeight);
-        ctx.fillStyle = "#0095DD";
-        ctx.fill();
-        ctx.closePath();
-      }
-    }
-  }
-}
-
 
 //Event Listeners/KeyBindings
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 document.addEventListener("mousemove", mouseMoveHandler, false);
+document.addEventListener("click", mouseClickHandler, false);
 
 function mouseMoveHandler(e){
   var relativeX = e.clientX - canvas.offsetLeft;
   if(relativeX > 0 && relativeX < canvas.width) {
       paddleX = relativeX - paddleWidth/2;
+  }
+}
+
+function mouseClickHandler(e){
+  if() {
+      //launchBall
   }
 }
 
